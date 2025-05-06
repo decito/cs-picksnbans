3d908db376bf5e439af585a00e18a77182c3d062
Decio Neto <deciom.neto10@outlook.com>
2025-05-06 00:39:57 -0300

    update tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: 'hsl(var(--r-green) / <alpha-value>)',
-      },
-    },
-  },
+        primary: 'hsl(var(--r-green) / <alpha-value>)'
+      }
+    }
+  }
 }
