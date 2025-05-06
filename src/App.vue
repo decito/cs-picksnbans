7fade4e0d66769f7ed644ad74bcecb31787c4795
Decio Neto <deciom.neto10@outlook.com>
2025-05-06 00:06:05 -0300

    start of project update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+import MapsContainer from '@/components/MapsContainer.vue'
+</script>
+
 <template>
   <AppHeader />
-
   <MapsContainer />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      imports: 'vue'
-    }),
-    Components()
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
